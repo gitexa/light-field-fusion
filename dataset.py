671a13fb4da87066e9f455f3537e7fb6ab5425b0
Alexander Haas <haas.alexanderjulian@gmail.com>
2020-10-22 16:07:39 +0200

    Added testing pipeline and corrected errors

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 import torch
 import os
-import preprocessing
+import dataset_processing
 import configparser
 from PIL import Image
 import torchvision.transforms as transforms
@@ -29,7 +29,7 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         
         # Generate one sample of data: 2x5 PSVS with hold-out target-view 
-        sample = dict()
+        sample = {}
         
         # Select sample
         id = self.ids[index]
@@ -39,65 +39,94 @@ class Dataset(torch.utils.data.Dataset):
         scene_dir = params[0]
         mpi_1_coords = params[1]
         mpi_2_coords = params[2]
-        mpi_1_psvs = get_psvs(mpi_1_coords, scene_dir)
-        mpi_2_psvs = get_psvs(mpi_2_coords, scene_dir)
-        target_image_coords = get_target_image_coords(mpi_1_coords, mpi_2_coords)
+        mpi_1_psvs = self.get_psvs(mpi_1_coords, scene_dir)
+        mpi_2_psvs = self.get_psvs(mpi_2_coords, scene_dir)
+        target_image_coords = self.get_target_image_coords(mpi_1_coords, mpi_2_coords)
 
 
         # Get config data
-        baselineMM, focalLength, sensorWidth = load_config_from_file(scene_dir)
+        baselineMM, focalLength, sensorWidth = self.load_config_from_disk(scene_dir)
         
         # Scene parameters
+        sample['sample_id'] = id
         sample['scene_id'] = scene_dir
         sample['baselineMM'] = baselineMM
         sample['focalLength'] = focalLength
         sample['sensorWidthMM'] = sensorWidth
 
         # PSVs, target_image and poses
-        sample['psvs'] = torch.squeeze(torch.stack((mpi_1_psvs, mpi_2_psvs), dim=0)
-        sample['psv_center_1_pose'] = mpi_1_coords
-        sample['psv_center_2_pose'] = mpi_2_coords
-        sample['target_image'] = get_target_image_from_disk(target_image_coords)
+        sample['psvs'] = torch.stack((mpi_1_psvs, mpi_2_psvs), dim=0)
+        sample['psv_center_1_pose'] = dataset_processing.string2coords(mpi_1_coords)
+        sample['psv_center_2_pose'] = dataset_processing.string2coords(mpi_2_coords)
+        sample['target_image'] = self.get_target_image_from_disk(scene_dir, target_image_coords)
         sample['target_image_pose'] = target_image_coords
 
         return sample
+    
+    
+    def get_target_image_coords(self, mpi_1_coords, mpi_2_coords):
+
+        target_coords = ''
+
+        mpi_1_coords = dataset_processing.string2coords(mpi_1_coords)
+        mpi_1_row = mpi_1_coords[0]
+        mpi_1_column = mpi_1_coords[1]
+        mpi_2_coords = dataset_processing.string2coords(mpi_2_coords)
+        mpi_2_row = mpi_2_coords[0]
+        mpi_2_column = mpi_2_coords[1]
+
+        if(mpi_1_row != mpi_2_row):
+            diff = int(abs(mpi_1_row - mpi_2_row)/2)
+            if(mpi_1_row > mpi_2_row):
+                target_coords = (mpi_1_row-diff,mpi_1_column)
+            elif(mpi_1_row < mpi_2_row):
+                target_coords = (mpi_2_row-diff,mpi_2_column)
+        elif(mpi_1_column != mpi_2_column):
+            diff = int(abs(mpi_1_column - mpi_2_column)/2)
+            if(mpi_1_column > mpi_2_column): 
+                target_coords = (mpi_1_row, mpi_1_column-diff)
+            elif(mpi_1_column < mpi_2_column):
+                target_coords = (mpi_2_row, mpi_2_column-diff)
+        
+        return target_coords
     
     # Function to get 5 PSVS for a center MPI position 
     # Input: MPI center position 
     # Output: Pytorch tensor with 4 nearest neighbour PSVs and the center 
 
-    def get_psvs(mpi_coords, scene_dir):
+    def get_psvs(self, mpi_coords, scene_dir):
 
-        coords = preprocessing.string2coords(mpi_coords)
+        coords = dataset_processing.string2coords(mpi_coords)
         row = coords[0]
-        column coords[1]
+        column = coords[1]
         
         # get psvs of first mpi_pose (upperleft, ...)
-        psv_1_center_coords = (row, column)
-        psv_1_ul_coords = (row-1, column-1)
-        psv_1_ur_coords = (row-1, column+1)
-        psv_1_ll_coords = (row+1, column-1)
-        psv_1_lr_coords = (row+1, column+1)
+        psv_center_coords = (row, column)
+        psv_ul_coords = (row-1, column-1)
+        psv_ur_coords = (row-1, column+1)
+        psv_ll_coords = (row+1, column-1)
+        psv_lr_coords = (row+1, column+1)
 
-        psvs_1_center = load_psvs_from_file(psv_1_center_coords, scene_dir)
-        psvs_1_ul = load_psvs_from_file(psv_1_ul_coords, scene_dir)
-        psvs_1_ur = load_psvs_from_file(psv_1_ur_coords, scene_dir)
-        psvs_1_ll = load_psvs_from_file(psv_1_ll_coords, scene_dir)
-        psvs_1_lr = load_psvs_from_file(psv_1_lr_coords, scene_dir)
+        psv_center = self.load_psvs_from_disk(psv_center_coords, scene_dir)
+        psv_ul = self.load_psvs_from_disk(psv_ul_coords, scene_dir)
+        psv_ur = self.load_psvs_from_disk(psv_ur_coords, scene_dir)
+        psv_ll = self.load_psvs_from_disk(psv_ll_coords, scene_dir)
+        psv_lr = self.load_psvs_from_disk(psv_lr_coords, scene_dir)
 
-        psvs = torch.squeeze(torch.stack((psvs_1_center, psvs_1_ul, psvs_1_ur, psv_1_ll_coords, psvs_1_lr), dim=0))
+        psvs = torch.cat((psv_center, psv_ul, psv_ur, psv_ll, psv_lr), dim=0)
 
         return psvs
 
-    def load_psvs_from_file(psv_coords, scene_dir):
-        path = self.path_to_data + '/' + scene_dir + '/' + 'psv_' + preprocessing.coords2string(psv_coords) + '.pt'
-        psv = torch.load(path)
+    def load_psvs_from_disk(self, psv_coords, scene_dir):
+        path = self.path_to_data + '/' + scene_dir + '/' + 'psv_' + dataset_processing.parse_coordinates2camsnumbering(psv_coords) + '.pt'
 
-        # TODO: assert psv to be psv/tensor
+        # TODO: load correct PSV and assert psv to be psv/tensor
+        #psv = torch.load(path)
+        psv = torch.rand(3,24,24,8)
 
         return psv
     
-    def load_config_from_file(scene_dir):
+    def load_config_from_disk(self, scene_dir):
         path = self.path_to_data + '/' + scene_dir + '/parameters.cfg'
         config = configparser.RawConfigParser()
         config.read(path)
@@ -107,35 +136,10 @@ class Dataset(torch.utils.data.Dataset):
 
         return baselineMM, focalLength, sensorWidth
 
-    def get_target_image_coords(mpi_1_coords, mpi_2_coords):
-
-        target_coords = ''
-
-        mpi_1_coords = preprocessing.string2coords(mpi_1_coords)
-        mpi_1_row = mpi_1_coords[0]
-        mpi_1_column = mpi_1_coords[1]
-        mpi_2_coords = preprocessing.string2coords(mpi_2_coords)
-        mpi_2_row = mpi_2_coords[0]
-        mpi_2_column = mpi_2_coords[1]
-
-        if(mpi_1_row != mpi_2_row):
-            diff = abs(mpi_1_row - mpi_2_row)/2
-            if(mpi_1_row > mpi_2_row):
-                target_coords = (mpi_1_row-diff,mpi_1_column)
-            elif(mpi_1_row < mpi_2_row):
-                target_coords = (mpi_2_row-diff,mpi_2_column)
-        elif(mpi_1_column != mpi_2_column):
-            diff = abs(mpi_1_column - mpi_2_column)/2
-            if(mpi_1_column > mpi_2_column): 
-                target_coords = (mpi_1_row, mpi_1_column-diff)
-            elif(mpi_1_column < mpi_2_column):
-                target_coords = (mpi_2_row, mpi_2_column-diff)
-        
-        return target_coords
     
-    def get_target_image_from_disk(target_image_coords):
+    def get_target_image_from_disk(self, scene_dir, target_image_coords):
         
-        path = self.path_to_data + '/' + scene_dir + '/' + 'input_Cam0' + preprocessing.coords2string(target_image_coords) +'.png' 
+        path = self.path_to_data + '/' + scene_dir + '/' + 'input_Cam0' + dataset_processing.coords2string(target_image_coords) +'.png' 
         pil_image = Image.open(path)
         pil2tensor = transforms.ToTensor()
         rgb_image = pil2tensor(pil_image)
--- dataset_processing.py
+++ dataset_processing.py
@@ -1,70 +1,107 @@
-import torch
-
-relative_path_to_scenes = 'less_data/'
+'Helper Methods'
 
 
-def get_nn(sample_mpi_poses):
+def parse_camsnumbering2coordinates(number):
+
+    int_num = 51
+    int_num = int(number)
+    column = int(int_num % 9)
+    row = int((int_num - column)/9)
+
+    coord = (row, column)
+
+    return coord
+
+# (row, column)
+def parse_coordinates2camsnumbering(coordinates):
     
-    # get psvs of first mpi_pose
-    psv_1_center = (sample_mpi_poses[0][0], sample_mpi_poses[0][0])
-    psv_1_ul = (sample_mpi_poses[0][0]-1, sample_mpi_poses[0][0]-1)
-    psv_1_ur = (sample_mpi_poses[0][0]-1, sample_mpi_poses[0][0]+1)
-    psv_1_ll = (sample_mpi_poses[0][0]+1, sample_mpi_poses[0][0]-1)
-    psv_1_lr = (sample_mpi_poses[0][0]+1, sample_mpi_poses[0][0]+1)
+    cam_number = ''
 
-    # get psvs of second mpi_pose
-    psv_2_center = (sample_mpi_poses[1][0], sample_mpi_poses[1][0])
-    psv_2_ul = (sample_mpi_poses[1][0]-1, sample_mpi_poses[1][0]-1)
-    psv_2_ur = (sample_mpi_poses[1][0]-1, sample_mpi_poses[1][0]+1)
-    psv_2_ll = (sample_mpi_poses[1][0]+1, sample_mpi_poses[1][0]-1)
-    psv_2_lr = (sample_mpi_poses[1][0]+1, sample_mpi_poses[1][0]+1)
+    row = int(coordinates[0])
+    column = int(coordinates[1])
+    number = 9 * row + column
 
-    # get psv tensors from file 
-    psvs_1 = torch.stack((psv_1_center, psv_1_ul, psv_1_ur, psv_1_ll, psv_1_lr), dim=0)
-    psvs_2 = torch.stack((psv_2_center, psv_2_ul, psv_2_ur, psv_2_ll, psv_2_lr), dim=0)
+    return '0' + str(number)
 
-    psvs = torch.squeeze(torch.stack((psvs_1, psvs_2), dim=0))
+
+# Function to transfrom coordinate tuples to string (1,1) --> 11
+# Input: coordinates 
+# Output: string 
+
+def coords2string(coords):
+    s = ''.join((str(coord) for coord in coords))
+    return s
+
+def string2coords(s):
+    l = list(str(s))
+    return (int(l[0]), int(l[1]))
+
+
+# Generate all IDs for the given scenario with fixed geometry in a 9x9 camera grid - 2 MPI positions as center of a cross with target_view in between
+# --> format of an ID is "sceneid_mpicoords1_mpicoordsposition2", e.g. "1JQ8tLwWMnJtSO_11_15"
+# total count 6*6*2
+# Input: all scenes
+# Output: IDs of all training-data
+
+def generate_all_ids(all_scenes):
     
-    return psvs
+    all_ids= []
+
+    for scene in all_scenes:
+        for mpi_1_row in range(1,8):
+            for mpi_1_column in range(1,8):
+                # First MPI coords
+                mpi_1 = (mpi_1_row, mpi_1_column)
+                # Get both samples 
+                mpi_2_opt1, mpi_2_opt2 = get_second_mpi_position(mpi_1_row, mpi_1_column)
+                # Generate two IDs and append to all_id list
+                if(mpi_2_opt1 != ''):
+                    id_1 = str(scene) + '_' + coords2string(mpi_1) + '_' + coords2string(mpi_2_opt1)
+                    all_ids.append(id_1)
+                if(mpi_2_opt2 != ''):
+                    id_2 = str(scene) + '_' + coords2string(mpi_1) + '_' + coords2string(mpi_2_opt2)
+                    all_ids.append(id_2)
     
+    return all_ids
 
 
+# Function to get 2 positions for the second MPI, given the first MPI
+# Input: row and colum coordinates of first MPI
+# Output: two possible coordinate-tuples (row,column) for the second MPI position
+def get_second_mpi_position(row, column):
 
-
-
-# Function to get 5 PSVS for each of 2 MPI target poses 
-def get_nn_psvs(row, column):
-
-    first_mpi_pose = (row, column)
+    second_mpi_pose_var1 = ''
+    second_mpi_pose_var2 = ''
     
     # select second MPI pose
     if(row<=3 and column <=3):
-        second_mpi_pose_var1 = (row, column+3)
-        second_mpi_pose_var2 = (column, row+3)
-    elif(row<=3 and column>3):
-        second_mpi_pose_var1 = (row, column-3)
-        second_mpi_pose_var2 = (column, row+3)
-    elif(row>3 and column<=3):
-        second_mpi_pose_var1 = (row, column+3)
-        second_mpi_pose_var2 = (column, row-3)
-    elif(row>3 and column>3):
-        second_mpi_pose_var1 = (row, column-3)
-        second_mpi_pose_var2 = (column, row-3)
+        second_mpi_pose_var1 = (row, column+4)
+        second_mpi_pose_var2 = (row+4, column)
+    elif(row<=3 and column>4):
+        second_mpi_pose_var1 = (row, column-4)
+        second_mpi_pose_var2 = (row+4, column)  
+    elif(row>4 and column<=3):
+        second_mpi_pose_var1 = (row, column+4)
+        second_mpi_pose_var2 = (row-4, column)
+    elif(row>4 and column>4):
+        second_mpi_pose_var1 = (row, column-4)
+        second_mpi_pose_var2 = (row-4, column)
+    # edge case row/column==4
+    elif(row==4 and column<=3):
+        second_mpi_pose_var1 = (row, column+4)
+    elif(row==4 and column>4):
+        second_mpi_pose_var1 = (row, column-4)
+    elif(row<=3 and column==4):
+        second_mpi_pose_var1 = (row+4, column)
+    elif(row>4 and column==4):
+        second_mpi_pose_var1 = (row-4, column)
+    
+    
+    #sample_1 = (first_mpi_pose, second_mpi_pose_var1)
+    #sample_2 = (first_mpi_pose, second_mpi_pose_var2)
+    #nn_sample_1 = get_nn(sample_1)
+    #nn_sample_2 = get_nn(sample_2)
 
-    sample_1 = (first_mpi_pose, second_mpi_pose_var1)
-    sample_2 = (first_mpi_pose, second_mpi_pose_var2)
-
-    nn_sample_1 = get_nn(sample_1)
-    nn_sample_2 = get_nn(sample_2)
-
-    return sample
-
-
-w, h = 9, 9;
-all_views = [[0 for x in range(w)] for y in range(h)] 
-
-for row in range(9):
-    for column in range(9):
-        allviews[row][column] = get_nn_psvs(row, column)
+    return second_mpi_pose_var1, second_mpi_pose_var2
 
         
