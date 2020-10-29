b279dc778aed5bbd9f18b1f638756909ad9fe581
NathanHuetsch <70834402+NathanHuetsch@users.noreply.github.com>
2020-10-29 15:46:47 +0100

    network_into_mpi

--- processing.py
+++ processing.py
@@ -384,7 +384,27 @@ def save_images(relative_path_to_results, target_image, predicted_target_image, 
         
         
 
-
+def network_into_mpi(tensor, psvs):
+    
+    #assume tensor is shape (2, 5,512,512,8)
+    #assume psvs is shape (2,5,3,512,512)
+    
+    mpis = torch.zeros((2,4,512,512,8))
+    
+    mpis[:,0] = tensor[:,0]
+    
+    softmax_input1 = torch.stack( [torch.zeros((512,512,8)), tensor[0,1], tensor[0,2], tensor[0,3], tensor[0,4]], dim=0)
+    softmax_output1 = torch.nn.functional.softmax(softmax_input1, dim=0)
+    
+    softmax_input2 = torch.stack( [torch.zeros((512,512,8)), tensor[1,1], tensor[1,2], tensor[1,3], tensor[1,4]], dim=0)
+    softmax_output2 = torch.nn.functional.softmax(softmax_input2, dim=0)
+    
+    for d in range(8):
+        mpis[0,1:,:,:,d] = torch.sum(psvs[0,:,:,:,:] * softmax_output1[:,None,:,:,d], dim=0)
+        mpis[1,1:,:,:,d] = torch.sum(psvs[1,:,:,:,:] * softmax_output2[:,None,:,:,d], dim=0)
+                       
+    return mpis
+    
     
     
     
